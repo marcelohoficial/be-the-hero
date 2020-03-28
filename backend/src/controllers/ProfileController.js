aa4f20cc0a336ff0f28a35d1664ecb60e9942aae
marcelohoficial <marcelo.12.musico@gmail.com>
2020-03-28 18:47:40 -0300

    Front-end e Back-end Connected

--- backend/src/controllers/ProfileController.js
+++ backend/src/controllers/ProfileController.js
@@ -2,7 +2,7 @@ const connection = require('../database/connection');
 
 module.exports = {
   async index(req, res) {
-    const ong_id = req.headers.autorization;
+    const ong_id = req.headers.authorization;
 
     const incidents = await connection('incidents')
       .where('ong_id', ong_id)
